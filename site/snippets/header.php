70cd9762494e8638a31318da0209c7e79290b905
Nils Hendriks <nils@nilshendriks.com>
2012-06-25 11:32:41 +0900

    Default theme made responsive ready
    Added media queries to have a starting point for a responsive theme.
    Replaced logo image with text for 'kirby' name and SVG version of icon.
    
    Signed-off-by: Nils Hendriks <nils@nilshendriks.com>

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
+
   <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="shortcut icon" href="<?php echo url('assets/images/favicon.png') ?>" type="image/png" />
+  <link rel="icon" href="<?php echo url('assets/images/favicon.png') ?>" type="image/png" />
+  <link rel="apple-touch-icon" href="<?php echo u('assets/images/apple-touch-icon.png') ?>" />
 
   <?php echo css('assets/styles/styles.css') ?>
+  <?php echo js('assets/scripts/default.js') ?>
 
 </head>
 
 <body>
 
   <header>
-    <h1><a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/logo.png') ?>" width="115" height="41" alt="<?php echo html($site->title()) ?>" /></a></h1>
+    <h1><a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/kirbyicon.svg') ?>" width="37" height="41" alt="<?php echo h($site->title()) ?>" /><span>kirby</span></a></h1>
   </header>
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,17 @@
 <?php snippet('header') ?>
-<?php snippet('menu') ?>
-<?php snippet('submenu') ?>
 
-<section class="content">
+<?php
+
+// find the open/active page on the first level
+$open  = $pages->findOpen();
+$items = ($open) ? $open->children()->visible() : false;
+
+?>
+<?php if($items && $items->count()): ?>
+	<section class="content subpage">
+<?php else: ?>
+	<section class="content">
+<?php endif ?>
 
   <article>
     <h1><?php echo html($page->title()) ?></h1>
@@ -10,5 +19,8 @@
   </article>
 
 </section>
+
+<?php snippet('submenu') ?>
+<?php snippet('menu') ?>
 
 <?php snippet('footer') ?>
